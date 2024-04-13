8a045249092b57ac7b00af6b689e8a4a2f665fbc
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2024-04-13 21:23:45 +0300

    add github origin

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 const whitelist = [
 	"http://localhost:5173",
 	"http://127.0.0.1:5173",
-	"https://your-gh-pages-url",
+	"https://gitstudentsem.github.io",
 ];
 
 app.use(cors({ origin: whitelist }));
