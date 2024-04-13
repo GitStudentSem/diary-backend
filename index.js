d7e4191a9d7e667edfb57ebf4d0e81d7e9006fc4
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2024-04-13 09:57:53 +0300

    add headers

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
+	next();
+});
+
 export const db = new JsonDB(new Config("myDataBase", true, true, "/"));
 
 app.post(
